6e9cb0194c3d155bf1fb07a77ea3f6cbff27abcf
Agustin Gimenez <agimenez@fie.undef.edu.ar>
2025-10-06 01:05:01 -0300

    Update 3 nodos

--- run_nodo5000.bat
+++ run_nodo5000.bat
@@ -1,17 +1,20 @@
 @echo off
-title Nodo Sucursal5001 (5001)
+title Nodo Sucursal5000 (5000)
 echo ===============================
-echo Iniciando Sucursal5001 en 26.39.171.184:5001
+echo Iniciando Sucursal5000 en 26.60.177.15:5000
 echo ===============================
 
+REM Activar entorno virtual
 call .venv_win\Scripts\activate
 
+REM Variables de entorno
 set HOST=0.0.0.0
-set PORT=Sucursal5001
-set NODE_NAME=Principal
-set PUBLIC_HOST=26.39.171.184
+set PORT=5000
+set NODE_NAME=Sucursal5000
+set PUBLIC_HOST=26.60.177.15
 set PEERS=26.60.177.15:5000,26.39.171.184:5001,26.32.162.255:5002
 
+REM Ejecutar Flask
 python run.py
 
 pause
--- run_nodo5002.bat
+++ run_nodo5002.bat
@@ -1,15 +1,15 @@
 @echo off
-title Nodo Sucursal5001 (5001)
+title Nodo Sucursal5002 (5002)
 echo ===============================
-echo Iniciando Sucursal5001 en 26.39.171.184:5001
+echo Iniciando Sucursal5002 en 26.32.162.255:5002
 echo ===============================
 
 call .venv_win\Scripts\activate
 
 set HOST=0.0.0.0
-set PORT=Sucursal5001
-set NODE_NAME=Principal
-set PUBLIC_HOST=26.39.171.184
+set PORT=5002
+set NODE_NAME=Sucursal5002
+set PUBLIC_HOST=26.32.162.255
 set PEERS=26.60.177.15:5000,26.39.171.184:5001,26.32.162.255:5002
 
 python run.py
