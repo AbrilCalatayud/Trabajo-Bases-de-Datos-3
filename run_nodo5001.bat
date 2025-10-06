8cd9c2b052bf3741321450ffb75564238802c9d3
Agustin Gimenez <agimenez@fie.undef.edu.ar>
2025-10-06 01:06:20 -0300

    Update nodos

--- run_nodo5001.bat
+++ run_nodo5001.bat
@@ -7,8 +7,8 @@ echo ===============================
 call .venv_win\Scripts\activate
 
 set HOST=0.0.0.0
-set PORT=Sucursal5001
-set NODE_NAME=Principal
+set PORT=5001
+set NODE_NAME=Sucursal5001
 set PUBLIC_HOST=26.39.171.184
 set PEERS=26.60.177.15:5000,26.39.171.184:5001,26.32.162.255:5002
 
